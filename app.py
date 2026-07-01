3f279523256d5419023f9d1796dde93ec80ed4db
hos5 <hosam1422333@gmail.com>
2026-07-01 16:46:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -717,7 +717,7 @@ with st.sidebar:
     )
     st.markdown("---")
     page = st.radio(
-        "القوائم",
+        "Dashboard",
         [
             "Overview",
             "Risk Prediction",
